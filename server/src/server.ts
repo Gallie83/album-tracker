8a2e7f10c995d16ddbda88f8b5042ddc2a662be6
Gallie83 <keving2913@hotmail.com>
2024-11-09 23:59:04 -0800

    Project set up with client+server folders, MERN stack set up with typescript and tsx

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const uri: string =
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-app';
+
+(async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to the database');
+    } catch(error) {
+        console.error(error);
+    }
+})();
+
+const PORT: string | number = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
