f867fac7daadf3d0720f5940452c06316a2cbb0c
Gallie83 <keving2913@hotmail.com>
2024-11-10 13:55:31 -0800

    Testing API search queries using musicbrainz.org to search for albums

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<unknown>({});
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+  
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://musicbrainz.org/ws/2/release?query=release:${encodeURIComponent(searchValue)}&fmt=json`);
+      const data = await response.json()
+      setSearchResults(data);
+      console.log(searchResults)
+    } catch(error) {
+      console.log('Error fetching data:', error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Album Search</h1>
+    <input 
+      type="text"
+      value={searchValue} 
+      onChange={handleInputChange}
+    />
+    <button onClick={handleSearch}>Search</button>
     </>
   )
 }
