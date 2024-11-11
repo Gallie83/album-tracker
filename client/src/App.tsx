15406574257f28ed4b8085c63894b3d5da562b88
Gallie83 <keving2913@hotmail.com>
2024-11-11 10:10:22 -0800

    Artist Name now appears on homepage after mapping searchResults to state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,10 +31,18 @@ function App() {
   
   const handleSearch = async () => {
     try {
+      // Searches for albums based on user input for searchValue
       const response = await fetch(`https://musicbrainz.org/ws/2/release?query=release:${encodeURIComponent(searchValue)}&fmt=json`);
       const data = await response.json()
-      setSearchResults({results: data.releases});
-      console.log(searchResults)
+      // Maps results to searchResults state to match SearchResults interface
+      setSearchResults({
+        results: data.releases.map((release: { [x: string]: string; id: string; title: string; date: string; }) => ({
+          id: release.id,
+          title: release.title,
+          date: release.date,
+          artistCredit: release['artist-credit'], 
+        }))
+      });
       console.log(searchResults?.results)
     } catch(error) {
       console.log('Error fetching data:', error);
@@ -44,6 +52,8 @@ function App() {
   return (
     <>
     <h1>Album Search</h1>
+    
+    {/* Search input for users to search for albums */}
     <input 
       type="text"
       value={searchValue} 
@@ -56,7 +66,7 @@ function App() {
       searchResults.results.map((album) => (
         <div key={album.id}>
           <h2>{album.title}</h2>
-          <p>Artist: {album.artistCredit?.[0]?.name || 'No artist information available'}</p>
+          <p>Artist: {album.artistCredit[0]?.name || 'No artist information available'}</p>
           <p>Release Date: {album.date}</p>
         </div>
       ))
