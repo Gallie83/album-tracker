0b4ffe32fd6f38c09e1faaeaeb9f680355b0a598
Gallie83 <keving2913@hotmail.com>
2024-11-17 13:05:05 -0800

    Removed base styles from index.css as they were restricting the albumInfo size

--- client/src/components/AlbumInfo.tsx
+++ client/src/components/AlbumInfo.tsx
@@ -10,30 +10,32 @@ interface AlbumInfo {
     imageUrl: string,
     trackList: {
         name: string,
-        // rank: number,
     }[], 
 }
 
 function AlbumInfo() {
     const params = useParams<{artistName:string , albumName: string, albumId: string}>();
     const [album, setAlbum] = useState<AlbumInfo>();
+    const [open, setOpen] = useState(false);
+
 
     
     // const name = params.artistName;
-    const albumInfo = async (artist: string, album: string, albumId: string) => {
+    const albumInfo = async (artist: string, album: string) => {
             try {
         const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key=${apiKey}&artist=${encodeURIComponent(artist)}&album=${encodeURIComponent(album)}&format=json`);
         const data = await response.json()
         if(data.album) {
             const fetchedAlbum: AlbumInfo = {
-                id : albumId,
+                id : data.album.mbid,
                 title: album,
                 artist: artist,
                 imageUrl: data.album.image?.find(
                     (img: { "#text": string; size: string }) => img.size === "extralarge"
                   )?.["#text"] || "",
-                trackList: data.album.tracks.track.map((track: {name: string}) => ({
-                    name: track.name
+                trackList: data.album.tracks.track.map((track: {name: string, rank:number}) => ({
+                    name: track.name,
+                    rank: track.rank,
                 })) || []
             }
             setAlbum(fetchedAlbum);
@@ -45,28 +47,52 @@ function AlbumInfo() {
 }
 
     useEffect(() => {
-        if(params.artistName && params.albumName && params.albumId)
-        albumInfo(params.artistName, params.albumName, params.albumId)
+        if(params.artistName && params.albumName )
+        albumInfo(params.artistName, params.albumName)
     }, [params])
         
     return(
         <>
         {album ? (
+            
+          <div key={album.id} className="flex items-center bg-white border border-gray-200 rounded-lg md:flex-row">
+              <img className="w-full rounded-t-lg md:rounded-none md:rounded-s-lg" src={album.imageUrl} alt="Album Art"></img>
+              <div className="flex flex-col justify-between w-full bg-brown-600 px-16 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{album.artist}</p>
 
-            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={album.imageUrl} alt=""></img>
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{album.artist}</p>
-                    <hr />
-                    {album.trackList.map((track) => (
-                        <p className="text-black">{track.name}</p>
-                    ))}
-                </div>
-            </div>
-            ) : ( <p>ERROR</p> )}
+          {/* Track List Accordion */}
+          <div className="w-full">
+              <input
+                id="expandCollapse"
+                checked={open}
+                type="checkbox"
+                className="peer sr-only"
+                />
+              <label
+                htmlFor="expandCollapse"
+                className="
+                w-full flex justify-center items-center bg-blue-100
+                hover:bg-blue-500
+                transition-colors duration-1000 ease-in-out"
+                onClick={() => setOpen(!open)}
+                >
+                Track List
+              </label>
+              <div
+                className=
+                    "overflow-hidden h-0 bg-slate-300 peer-checked:h-[200px] peer-checked:overflow-scroll transition-[height] duration-1000 ease-in-out">
+                <p className="text-black">
+                          {album.trackList.map((track, index) => (
+                              <p key={index} className="text-black">{index+1}. {track.name}</p>
+                          ))}
+                </p>
+              </div>
+          </div>
+       </div>
+   </div>
+   ) : ( <p>ERROR</p> )}
         </>
-
     )
 }
 
