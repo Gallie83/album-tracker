e7ceaae4764a88de02c8f47a74acdb0705c8c746
Gallie83 <keving2913@hotmail.com>
2024-11-18 09:48:20 -0800

    Removed unnessasary padding on rootso albumInfo is max 100vh

--- client/src/components/AlbumInfo.tsx
+++ client/src/components/AlbumInfo.tsx
@@ -54,43 +54,43 @@ function AlbumInfo() {
     return(
         <>
         {album ? (
-            
-          <div key={album.id} className="flex items-center bg-white border border-gray-200 rounded-lg md:flex-row">
-              <img className="w-full rounded-t-lg md:rounded-none md:rounded-s-lg" src={album.imageUrl} alt="Album Art"></img>
-              <div className="flex flex-col justify-between w-full bg-brown-600 px-16 leading-normal">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{album.artist}</p>
+          // Outer div
+          <div className="flex items-center justify-center h-screen overflow-hidden">
+            <div key={album.id} className="flex w-11/12 items-center bg-pink-300 border border-gray-200 rounded-lg md:flex-row p-5">
+              <img className="w-10/12 p-5 rounded-t-lg md:rounded-none md:rounded-s-lg" src={album.imageUrl} alt="Album Art"></img>
+              <div className="flex flex-col justify-between h-full w-full bg-brown-600 px-16 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{album.artist}</p>
 
-          {/* Track List Accordion */}
-          <div className="w-full">
-              <input
-                id="expandCollapse"
-                checked={open}
-                type="checkbox"
-                className="peer sr-only"
-                />
-              <label
-                htmlFor="expandCollapse"
-                className="
-                w-full flex justify-center items-center bg-blue-100
-                hover:bg-blue-500
-                transition-colors duration-1000 ease-in-out"
-                onClick={() => setOpen(!open)}
-                >
-                Track List
-              </label>
-              <div
-                className=
-                    "overflow-hidden h-0 bg-slate-300 peer-checked:h-[200px] peer-checked:overflow-scroll transition-[height] duration-1000 ease-in-out">
-                <p className="text-black">
-                          {album.trackList.map((track, index) => (
-                              <p key={index} className="text-black">{index+1}. {track.name}</p>
-                          ))}
-                </p>
+              {/* Track List Accordion */}
+                <div className="w-full">
+                    <input
+                      id="expandCollapse"
+                      checked={open}
+                      type="checkbox"
+                      className="peer sr-only"
+                      />
+                    <label
+                      htmlFor="expandCollapse"
+                      className="
+                      w-full flex justify-center items-center bg-blue-100
+                      hover:bg-blue-500
+                      transition-colors duration-1000 ease-in-out"
+                      onClick={() => setOpen(!open)}
+                      >
+                      Track List
+                    </label>
+                    <div
+                      className=
+                          "overflow-hidden h-0 bg-slate-300 peer-checked:h-[200px] peer-checked:overflow-scroll transition-[height] duration-1000 ease-in-out">
+                                {album.trackList.map((track, index) => (
+                                    <p key={index} className="text-black">{index+1}. {track.name}</p>
+                                ))}
+                    </div>
+                </div>
               </div>
+            </div>
           </div>
-       </div>
-   </div>
    ) : ( <p>ERROR</p> )}
         </>
     )
