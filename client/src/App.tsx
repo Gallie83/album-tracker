d31508394b975b49804bc1fa4768ee563a24b4f7
Gallie83 <keving2913@hotmail.com>
2024-11-24 21:13:11 -0800

    Implemented Carousel to display each genres albums, but carousels have no content showing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -168,23 +168,69 @@ useEffect(() => {
   </div>
 ) : (
   // Display genres with best albums if no search results
+  // Object.entries(albumsByGenre).map(([genre, albums]) => (
+  //   <div className="block border border-white my-2 rounded" key={genre}>
+  //     <h2>{genre.toUpperCase()}</h2>
+  //     <div className="flex overflow-x-auto gap-5">
+  //       {albums.map((album) => (
+  //         <Link
+  //           to={`/album-info/${album.artist}/${album.title}`}
+  //           className="bg-slate-500 p-53 m-3 rounded-lg"
+  //           key={album.id}
+  //         >
+  //           <img className="w-48  object-cover" src={album.imageUrl} alt="Album art" />
+  //           <h2 className="font-bold">{album.title}</h2>
+  //           <p>Artist: {album.artist}</p>
+  //         </Link>
+  //       ))}
+  //     </div>
+  //   </div>
+  // ))
+
+  
+  // Display genres with best albums if no search results
   Object.entries(albumsByGenre).map(([genre, albums]) => (
-    <div className="block border border-white my-2 rounded" key={genre}>
-      <h2>{genre.toUpperCase()}</h2>
-      <div className="flex overflow-x-auto gap-5">
+  <div id="controls-carousel" className="relative w-full" data-carousel="static">
+  <h1>{genre.toUpperCase()}</h1>
+    {/* Carousel wrapper */}
+    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+         {/* Item 1 */}
+        <div className="hidden duration-700 bg-pink-300 h-10 ease-in-out" data-carousel-item>
+        <div className="flex overflow-x-auto bg-pink-300 gap-5">
         {albums.map((album) => (
           <Link
             to={`/album-info/${album.artist}/${album.title}`}
-            className="bg-slate-500 p-3 m-3 rounded-lg"
+            className="bg-slate-500 p-53 m-3 rounded-lg"
             key={album.id}
           >
-            <img className="w-96 h-96  object-cover" src={album.imageUrl} alt="Album art" />
+            <img className="w-48  object-cover" src={album.imageUrl} alt="Album art" />
             <h2 className="font-bold">{album.title}</h2>
             <p>Artist: {album.artist}</p>
           </Link>
         ))}
       </div>
+      </div>
     </div>
+
+
+    {/* Slider controls */}
+    <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span className="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span className="sr-only">Next</span>
+        </span>
+    </button>
+</div>
   ))
 )}
 
