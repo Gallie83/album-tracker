b86b428003385885700e1def4cc4bbb89a2f34cd
Gallie83 <keving2913@hotmail.com>
2024-12-04 18:06:39 -0800

    Move searchbar and title to separate component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useRef } from 'react'
 import './App.css'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
+import Searchbar from './components/Searchbar';
+
 const apiKey = import.meta.env.VITE_APP_API_KEY;
-
-
 
 // Interfaces
 interface Album {
@@ -22,18 +22,11 @@ interface AlbumsByGenre {
 
 function App() {
   
-  const [searchValue, setSearchValue] = useState<string>('');
   const [albumsByGenre, setAlbumsByGenre] = useState<AlbumsByGenre>({});
   const [tags, setTags] = useState<string[]>([])
 
   // const carouselRef = useRef({})
   const carouselRefs = useRef<Record<string, HTMLDivElement | null>>({});
-  const navigate = useNavigate()
-
-  // Tracks searchValue input
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(event.target.value);
-  };
 
   useEffect(() => {
     // Fetches Genre tags and stores them in state
@@ -96,13 +89,6 @@ function App() {
   fetchTopAlbums()
   }, [tags])
 
-  const searchRedirect = (event: React.FormEvent) => {
-    event.preventDefault();
-    if(searchValue.trim()) {
-      navigate(`search/${searchValue}`)
-    }
-  }
-
   // Scrolls carousel using left+right buttons 
   const scrollCarousel = (genre: string, direction: "next" | "prev") => {
     const carousel = carouselRefs.current[genre];
@@ -121,39 +107,8 @@ function App() {
   return (
     <>
     {/* <Navbar /> */}
-    <Link to={'/'}>
-    <h1 className='text-9xl'>VYNYL</h1>
-    </Link>
 
-    {/* Search input for album searching */}
-    <form onSubmit={searchRedirect}  className="flex px-4 py-3 rounded-md border-2 overflow-hidden max-w-md mx-auto font-[sans-serif]">
-  
-      <input 
-        placeholder='Search for Albums'
-        type="text"
-        value={searchValue} 
-        onChange={handleInputChange}
-        className='w-full outline-none bg-transparent text-gray-600 text-sm'
-      />
-
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" className="fill-gray-600">
-        <path
-          d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-        </path>
-      </svg>
-      
-    </form>
-
-
-    <div className="grid grid-cols-12">
-
-    {tags.map((tag) => (
-      // <div className="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-gray-700 bg-gray-100">
-      //   <div className="text-xs font-normal leading-none max-w-full">{tag}</div>
-      // </div>
-      <p></p>
-    ))}
-    </div>
+    <Searchbar/>
 
     {/* Display genres with best albums  */}
     {Object.entries(albumsByGenre).map(([genre, albums]) => (
--- client/src/components/SearchPage.tsx
+++ client/src/components/SearchPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+
+import Searchbar from './Searchbar';
 
 const apiKey = import.meta.env.VITE_APP_API_KEY;
 
@@ -20,7 +22,6 @@ function SearchPage() {
   
   const [searchResults, setSearchResults] = useState<SearchResults>();
   const params = useParams<{searchQuery:string}>();
-  const navigate = useNavigate();
   
   const handleSearch = async (query: string) => {
     if(!query) return;
@@ -56,39 +57,9 @@ function SearchPage() {
     handleSearch(searchQuery)
   }, [params])
 
-  const searchRedirect = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const searchValue = event.currentTarget.search.value
-    if(searchValue.trim()) {
-      navigate(`/search/${searchValue}`, {replace: true});
-    }
-  }
-
   return (
     <>
-    <Link to={'/'}>
-    <h1 className='text-9xl'>VYNYL</h1>
-    </Link>
-
-    {/* Search input for album searching */}
-    <form onSubmit={searchRedirect}  className="flex px-4 py-3 rounded-md border-2 overflow-hidden max-w-md mx-auto font-[sans-serif]">
-  
-      <input 
-        placeholder='Search for Albums'
-        type="text"
-        name="search" 
-        defaultValue={params.searchQuery || ""}
-        className='w-full outline-none bg-transparent text-gray-600 text-sm'
-      />
-
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" className="fill-gray-600">
-        <path
-          d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-        </path>
-      </svg>
-      
-    </form>   
-
+  <Searchbar />
     {/* Check if searchResults exists and then map through releases */}
     {searchResults?.results ? (
   // Display search results in a grid
