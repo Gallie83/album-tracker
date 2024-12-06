bb1113441761f875387e2537f51b57034166ae84
Gallie83 <keving2913@hotmail.com>
2024-12-06 14:26:06 -0800

    Removed concurrently from the project and set up database, server now connects when run

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,17 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri: string =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-app';
+const uri: string = process.env.MONGODB_URI || "";
+console.log(uri)
 
-(async () => {
-    try {
-        await mongoose.connect(uri);
-        console.log('Connected to the database');
-    } catch(error) {
-        console.error(error);
-    }
-})();
+// MongoDB connection setup
+mongoose.connect(uri)
+.then(() => console.log("Connected to DB"))
+.catch(console.error);
 
 const PORT: string | number = process.env.PORT || 3000;
 
