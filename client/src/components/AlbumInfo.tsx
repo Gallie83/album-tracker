b86c78766550280fad0762d1f04f3e424248fba5
Gallie83 <keving2913@hotmail.com>
2024-12-28 16:48:01 -0800

    Created modals folder in components folder, and moved RatingModal.tsx inside

--- client/src/components/AlbumInfo.tsx
+++ client/src/components/AlbumInfo.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom"
 import Navbar from "./Navbar/Navbar";
 import Searchbar from "./Searchbar";
-
-import VinylImage from './Navbar/vinyl-image.png'
+import RatingModal from "./modals/RatingModal";
 // import { AuthContext } from "../contexts/AuthContext";
 
 const apiKey = import.meta.env.VITE_APP_API_KEY;
@@ -25,9 +24,6 @@ function AlbumInfo() {
   const [album, setAlbum] = useState<AlbumInfo>();
   const [open, setOpen] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
-
-  // State for ratings
-  const [hoverIndex, setHoverIndex] = useState(0);
 
   // const { isAuthenticated } = useContext(AuthContext)!;
   
@@ -184,44 +180,7 @@ function AlbumInfo() {
             </div>
 
             {/* Rating modal  */}
-            {modalOpen && ( 
-            <div data-modal-backdrop="static" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative p-4 w-full max-w-2xl max-h-full">
-                    {/* Modal content  */}
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        {/* Modal header  */}
-                        <div className="flex justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Rate the album
-                            </h3>
-                            {/* Close modal button */}
-                            <button type="button" onClick={() => setModalOpen(false)} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">X</button>
-                        </div>
-                        {/* Modal body  */}
-                        <div className="flex flex-row justify-center p-10">
-                          {[...Array(5)].map((_, index) => (
-                            <div
-                              key={index}
-                              className={`w-12 h-12 mx-2 flex items-center justify-center cursor-pointer ${
-                                hoverIndex >= index ? "bg-yellow-500" : "bg-yellow-100"
-                              }`}
-                              onMouseEnter={() => setHoverIndex(index)}
-                              onMouseLeave={() => setHoverIndex(-1)} // Reset hover index when not hovering
-                            >
-                              <img src={VinylImage} alt="Vinyl" className="w-8 h-8" />
-                            </div>
-                          ))}
-                        </div>
-                        
-                        {/* Modal footer  */}
-                        <div className="flex items-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Skip</button>
-                            <button type="button" className="ms-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Rate</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-)}
+            {modalOpen && ( <RatingModal></RatingModal> )}
 
           </div>
         </div>
